bcf1a3f37d29fb28b52664dc01400b40913f5c14
VikalpVerma31 <123748856+VikalpVerma31@users.noreply.github.com>
2023-12-27 17:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,541 @@
+const questions = [
+    {
+        question:"समाजशास्त्र के पिता या जनक हैं-",
+        answers: [
+            { text: "इमाइल दुर्खीम", correct: false},
+            { text: "ऑगस्त काम्ते", correct: true},
+            { text: "मैक्स वैबर", correct: false},
+            { text: "पारसन्स", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र के जनक काम्ते का जनम किस सन् में हुआ?",
+        answers: [
+            { text: "1857 में", correct: false},
+            { text: "1798 में", correct: true},
+            { text: "1899 में", correct: false},
+            { text: "1789 में।", correct: false},
+        ] 
+    },
+    {
+        question:"भारत में वर्तमान समाजशास्त्र का अध्ययन कब प्रारंभ हुआ?",
+        answers: [
+            { text: "1917", correct: false},
+            { text: "1919", correct: true},
+            { text: "1921", correct: false},
+            { text: "1923", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र की सर्वप्रथम पुस्तक कौन-सी है ?",
+        answers: [
+            { text: "अमूर्त होता है", correct: false},
+            { text: "सोसाइटी", correct: false},
+            { text: "ह्यूमन सोसाइटी", correct: false},
+            { text: "प्रिंसिपल्स ऑफ सोशियोलॉजी            ", correct: true},
+        ] 
+    },
+    {
+        question:"'Social Structure of Values'नामक पुस्तक के लेखक कौन है?        ",
+        answers: [
+            { text: "डी० एन० मजूमदार", correct: false},
+            { text: "के० एम० कपाड़िया", correct: false},
+            { text: "एस० एन० श्रीनिवास", correct: false},
+            { text: "राधाकमल मुखर्जी", correct: true},
+        ] 
+    },
+    {
+        question:"'समाजशास्त्र अन्य विज्ञानों की न तो दासी है और न ही स्वामिनी वरन् यह उसकी बहिन है' यह कथन किस विद्वान का है?",
+        answers: [
+            { text: "स्पेन्सर", correct: false},
+            { text: "मार्क्स", correct: false},
+            { text: "सोरोकिन", correct: true},
+            { text: "वार्न्स एवं बीकर", correct: false},
+        ] 
+    },
+    {
+        question:"'समस्त विज्ञान की एकता केवल उसकी पद्धति में है उसकी विषय-वस्तु में नहीं'",
+        answers: [
+            { text: "कार्ल पियर्सन", correct: true},
+            { text: "स्टुअर्ट चेज़", correct: false},
+            { text: "लुण्डबर्ग", correct: false},
+            { text: "बर्नार्ड", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र की सर्वप्रथम पुस्तक कौन-सी है?",
+        answers: [
+            { text: "सोसाइटी", correct: false},
+            { text: "सोशियोलॉजी", correct: false},
+            { text: "प्रिंसिपल ऑफ सोशियोलॉजी", correct: true},
+            { text: "उपरोक्त में से कोई नहीं", correct: false},
+        ] 
+    },
+    {
+        question:"समन्वयात्मक सिद्धांत के अनुसार समाज की प्रकृति-        ",
+        answers: [
+            { text: "सावयवी है", correct: true},
+            { text: "असामाजिक है", correct: false},
+            { text: "सामाजिक है", correct: false},
+            { text: "काल्पनिक है", correct: false},
+        ] 
+    },
+    {
+        question:"बीरस्टीड के अनुसार समाजशास्त्र एक विज्ञान है",
+        answers: [
+            { text: "आत्मनिर्भर", correct: false},
+            { text: "विशिष्ट", correct: false},
+            { text: "आदर्शात्मक", correct: false},
+            { text: "विशुद्ध", correct: true},
+        ] 
+    },
+    {
+        question:"What is Sociology पुस्तक किसने लिखी है?",
+        answers: [
+            { text: "वार्ड", correct: false},
+            { text: "बॉटोमोर", correct: false},
+            { text: "ऐलेक्स इंकलिस", correct: true},
+            { text: "सोरोकिन", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र के समन्वयात्मक प्रवर्तक हैं-",
+        answers: [
+            { text: "मैक्स वैबर", correct: false},
+            { text: "जिन्सबर्ग", correct: true},
+            { text: "गिडिंग्स", correct: false},
+            { text: "वीरकांत", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र के अंतर्गत हम अध्ययन करते हैं",
+        answers: [
+            { text: "सामाजिक समूहों का", correct: false},
+            { text: "सामाजिक प्रक्रियाओं का", correct: false},
+            { text: "सामाजिक संबंधों का", correct: false},
+            { text: "उपरोक्त सभी", correct: true},
+        ] 
+    },
+    {
+        question:"सन् 1888 में समाजशास्त्र शब्द का सर्वप्रथम प्रयोग किस विद्वान् ने किया?        ",
+        answers: [
+            { text: "ऑगस्त काम्ते", correct: true},
+            { text: "दुर्खीम", correct: false},
+            { text: "अरस्तू", correct: false},
+            { text: "पी० ए० सोरोकिन", correct: false},
+        ] 
+    },
+    {
+        question:"Sociology शब्द किन दो भाषाओं के शब्दों से मिलकर बना है?",
+        answers: [
+            { text: "लेटिन एवं ग्रीक", correct: true},
+            { text: "पीक एवं अंग्रेजी", correct: false},
+            { text: "लेटिन एवं अंग्रेजी", correct: false},
+            { text: "लेटिन एवं रूसी।", correct: false},
+        ] 
+    },
+    {
+        question:"इतिहास अतीत की घटनाओं का किस प्रकार अध्ययन करता है?",
+        answers: [
+            { text: "स्पष्ट रूप से", correct: false},
+            { text: "क्रमबद्ध रूप से", correct: true},
+            { text: "संगठित रूप से", correct: false},
+            { text: "उपरोक्त में से कोई नहीं", correct: false},
+        ] 
+    },
+    {
+        question:"'समाजशास्त्र एकमात्र सामाजिक विज्ञान है' यह विचार किस विद्वान का है?",
+        answers: [
+            { text: "ऑगस्त काम्ते", correct: true},
+            { text: "स्पेन्सर", correct: false},
+            { text: "वार्ड", correct: false},
+            { text: "सोरोकिन", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र एवं अर्थशास्त्र में जिस तथ्य का सामान्य रूप से अध्ययन किया जाता है, वह है-",
+        answers: [
+            { text: "माँग एवं पूर्ति का नियम", correct: false},
+            { text: "पारिवारिक व्यवस्था", correct: false},
+            { text: "श्रम कल्याण", correct: true},
+            { text: "बजट के सिद्धांत", correct: false},
+        ] 
+    },
+    {
+        question:"निम्नलिखित में से कौन-सा कारण जजमानी व्यवस्था के विघटन के लिए उत्तरदायी नहीं है-        ",
+        answers: [
+            { text: "बढ़ती हुई जनसंख्या", correct: false},
+            { text: "औद्योगीकरण", correct: false},
+            { text: "ग्रामीण समाज में पाई जाने वाली एकता", correct: true},
+            { text: "जमींदारी उन्मूलन", correct: false},
+        ] 
+    },
+    {
+        question:"समाजशास्त्र एक विज्ञान है क्योंकि इसमें उपयोग होता है-",
+        answers: [
+            { text: "ऐतिहासिक पद्धति का", correct: false},
+            { text: "वैज्ञानिक पद्धति का", correct: true},
+            { text: "सांख्यिकीय पद्धति का", correct: false},
+            { text: "तुलनात्मक पद्धति का", correct: false},
+        ] 
+    },
+    {
+        question:"किस समाजशास्त्री ने मानवशास्त्र एवं समाजशास्त्र को जुड़वां बहने माना है?",
+        answers: [
+            { text: "बेकर", correct: false},
+            { text: "ऑगस्त काम्ते", correct: false},
+            { text: "सोरोकिन", correct: false},
+            { text: "क्रोबर", correct: true},
+        ] 
+    },
+    {
+        question:"निम्नलिखित में से किस विद्वान ने अपनी पुस्तक 'Social Organization' में प्राथमिक समूह की अवधारणा को प्रतिपादित किया है?",
+        answers: [
+            { text: "जी० सी० होमन्स", correct: false},
+            { text: "सी० एच० कूले", correct: true},
+            { text: "बोगार्डस", correct: false},
+            { text: "जी० सिमेल", correct: false},
+        ] 
+    },
+    {
+        question:"किसी समूह की स्थापना किन व्यक्तियों द्वारा होती है ?",
+        answers: [
+            { text: "जिनके हित समान हो", correct: false},
+            { text: "जिनकी रुचि समान हो", correct: false},
+            { text: "जिनके पारिवारिक संबंध हो", correct: false},
+            { text: "अ और ब दोनों।", correct: true},
+        ] 
+    },
+    {
+        question:"सामाजिक समूह को स्थायी एवं अस्थायी समूह में किसने विभाजित किया है?",
+        answers: [
+            { text: "समनर", correct: false},
+            { text: "कूले", correct: false},
+            { text: "गिलिन और गिलिन", correct: true},
+            { text: "मर्टन", correct: false},
+        ] 
+    },
+    {
+        question:"प्राथमिक एवं द्वितीयक समूह का वर्गीकरण किसके द्वारा किया गया है?",
+        answers: [
+            { text: "ऑगवर्न", correct: false},
+            { text: "मैकाइवर", correct: false},
+            { text: "कूले", correct: true},
+            { text: "लुण्डबर्ग", correct: false},
+        ] 
+    },
+    {
+        question:"संदर्भ समूह 'शब्द' का प्रयोग सर्वप्रथम किसके द्वारा किया गया?        ",
+        answers: [
+            { text: "मर्टन", correct: false},
+            { text: "न्यूकॉम्ब", correct: false},
+            { text: "बोगार्डस", correct: false},
+            { text: "हाइमन", correct: true},
+        ] 
+    },
+    {
+        question:"निम्नलिखित में से अस्थायी समूह हैं",
+        answers: [
+            { text: "राज्य", correct: false},
+            { text: "भीड़", correct: true},
+            { text: "समाज", correct: false},
+            { text: "राष्ट्र", correct: false},
+        ] 
+    },
+    {
+        question:"सभ्यता के विकास के साथ-साथ समाज में समूह की संख्या-",
+        answers: [
+            { text: "घटती जाती है", correct: false},
+            { text: "बढ़ती जाती है", correct: true},
+            { text: "परिवर्तित होती है", correct: false},
+            { text: "प्रभावित हो जाती है", correct: false},
+        ] 
+    },
+    {
+        question:"संदर्भ समूह होता है",
+        answers: [
+            { text: "प्राथमिक समूह            ", correct: false},
+            { text: "द्वितीयक समूह", correct: true},
+            { text: "अन्तः समूह", correct: false},
+            { text: "उपर्युक्त में से कोई नहीं।", correct: false},
+        ] 
+    },
+    {
+        question:" निम्नलिखित में से सामाजिक समूह का लक्ष्य कौन-सा है?",
+        answers: [
+            { text: "सदस्यता", correct: true},
+            { text: "समानता", correct: false},
+            { text: "अनिवार्य सदस्यता", correct: false},
+            { text: "आदान-प्रदान के संबंध", correct: false},
+        ] 
+    },
+    {
+        question:"जब कभी दो या दो से अधिक व्यक्ति एक साथ मिलते हैं एक-दूसरे को प्रभावित करते हैं तो वे सामाजिक समूह का निर्माण करते हैं यह परिभाषा किस विद्वान द्वारा दी गई हैं?        ",
+        answers: [
+            { text: "मैकाइवर एवं पेज", correct: false},
+            { text: "जिन्सबर्ग", correct: false},
+            { text: "जिन्सबर्ग", correct: false},
+            { text: "ऑगबर्न एवं निमकॉफ", correct: true},
+        ] 
+    },
+    {
+        question:"'संदर्भ समूह नकारात्मक भी हो सकता है'यह कथन किसका है?",
+        answers: [
+            { text: "शैरिफ", correct: false},
+            { text: "क्लाइनबर्ग", correct: true},
+            { text: "हाइमैन", correct: false},
+            { text: "मर्टन", correct: false},
+        ] 
+    },
+    {
+        question:"निम्नलिखित में से कौन-सा एक समूह रक्त संबंध पर आधारित है",
+        answers: [
+            { text: "परिवार", correct: true},
+            { text: "क्रीड़ा समूह", correct: false},
+            { text: "पड़ोस", correct: false},
+            { text: "श्रोता समूह", correct: false},
+        ] 
+    },
+    {
+        question:"समाज में क्या पाया जाता है?",
+        answers: [
+            { text: "केवल सहयोग", correct: false},
+            { text: "केवल संघर्ष", correct: false},
+            { text: "सहयोग एवं संघर्ष", correct: true},
+            { text: "उपरोक्त में से कोई नहीं।", correct: false},
+        ] 
+    },
+    {
+        question:"वर्ग विहीन समाज की अवधारणा किस विद्वान ने प्रस्तुत की है?        ",
+        answers: [
+            { text: "मार्क्स", correct: true},
+            { text: "दुर्खीम", correct: false},
+            { text: "हीगल।", correct: false},
+            { text: "स्पेन्सर", correct: false},
+        ] 
+    },
+    {
+        question:"एक समाज",
+        answers: [
+            { text: "अमूर्त होता है", correct: true},
+            { text: "मूर्त होता है", correct: false},
+            { text: "मूर्त एवं अमूर्त दोनों", correct: false},
+            { text: "उपरोक्त में से कोई नहीं", correct: false},
+        ] 
+    },
+    {
+        question:"समाज सामाजिक संबंधों का जाल है, यह कथन किस विद्वान का है?",
+        answers: [
+            { text: "पारसन्स", correct: false},
+            { text: "मैकाइवर एवं पेज", correct: true},
+            { text: "जिन्सबर्ग", correct: false},
+            { text: "डेविस", correct: false},
+        ] 
+    },
+    {
+        question:"जहाँ जीवन है वहीं समाज है, यह कथन किस विद्वान का है?",
+        answers: [
+            { text: "वार्ड", correct: false},
+            { text: "मैकाइवर एवं पेज            ", correct: true},
+            { text: "गिलिन एवं गिलिन", correct: false},
+            { text: "गिडिंग्स", correct: false},
+        ] 
+    },
+    {
+        question:"समुदाय होता है-",
+        answers: [
+            { text: "मूर्त", correct: true},
+            { text: "निर्मित", correct: false},
+            { text: "अमूर्त", correct: false},
+            { text: "कल्पित", correct: false},
+        ] 
+    },
+    {
+        question:"'लघु समुदाय' की अवधारणा किसकी है?",
+        answers: [
+            { text: "किंग्सले डेविस", correct: false},
+            { text: "बोगार्डस", correct: false},
+            { text: "रॉबर्ट रेडफील्ड", correct: true},
+            { text: "जिन्सबर्ग।", correct: false},
+        ] 
+    },
+    {
+        question:"'Community' शब्द किस भाषा के दो शब्दों से मिलकर बना है?        ",
+        answers: [
+            { text: "लेटिन", correct: true},
+            { text: "ग्रीक", correct: false},
+            { text: "जर्मन", correct: false},
+            { text: "फ्रेंच।", correct: false},
+        ] 
+    },
+    {
+        question:"'सामुदायिक भावना' को समुदाय का आधार किसने बताया है?",
+        answers: [
+            { text: "समनर", correct: false},
+            { text: "मैकाइवर एवं पेज", correct: true},
+            { text: "टॉयलर", correct: false},
+            { text: "किंग्सले डेविस", correct: false},
+        ] 
+    },
+    {
+        question:"समाज एक जीव रचना के समान है, यह कथन किसका है?",
+        answers: [
+            { text: "स्पेन्सर", correct: true},
+            { text: "मार्क्स", correct: false},
+            { text: "बोगा", correct: false},
+            { text: "कूले", correct: false},
+        ] 
+    },
+    {
+        question:"'Modern Sociology' पुस्तक के लेखक कौन हैं?",
+        answers: [
+            { text: "गोल्डनर", correct: true},
+            { text: "प्रीन", correct: false},
+            { text: "समनर", correct: false},
+            { text: "बोगार्डस", correct: false},
+        ] 
+    },
+    {
+        question:"संस्था को अदिश नियमों का संग्रह कौन विद्वान मानता है?",
+        answers: [
+            { text: "कूले", correct: false},
+            { text: "डेविस", correct: false},
+            { text: "समनर", correct: false},
+            { text: "एण्डसरसन", correct: true},
+        ] 
+    },
+    {
+        question:"संपूर्ण संस्था की अवधारणा प्रस्तुत करने वाले विद्वान कौन हैं?",
+        answers: [
+            { text: "वेबर", correct: false},
+            { text: "दुर्खीम", correct: false},
+            { text: "मर्टन", correct: true},
+            { text: "ईस्माइल", correct: false},
+        ] 
+    },
+    {
+        question:"'हम समितियों के सदस्य होते हैं, संस्थाओं के नहीं' यह कथन किस विद्वान का है?        ",
+        answers: [
+            { text: "मर्टन", correct: false},
+            { text: "बोगार्डस", correct: false},
+            { text: "मैकाइवर एवं पेज", correct: true},
+            { text: "दुर्खीम", correct: false},
+        ] 
+    },
+    {
+        question:"समिति की सदस्यता-",
+        answers: [
+            { text: "अनिवार्य होती है", correct: false},
+            { text: "ऐच्छिक होती है", correct: true},
+            { text: "जन्म द्वारा होती है", correct: false},
+            { text: "उपर्युक्त में से कोई नहीं", correct: false},
+        ] 
+    },
+    {
+        question:"निम्न में से समिति कौन सी है?",
+        answers: [
+            { text: "ट्रेड यूनियन", correct: false},
+            { text: "राज्य", correct: false},
+            { text: "टेनिस क्लब", correct: false},
+            { text: "उपर्युक्त सभी", correct: true},
+        ] 
+    },
+    {
+        question:"समाज में सहयोग एवं संघर्ष पाया जाता है किंतु समिति में-",
+        answers: [
+            { text: "सहयोग एवं समानता", correct: true},
+            { text: "सहयोग व विरोध", correct: false},
+            { text: "केवल विरोध", correct: false},
+            { text: "केवल सहयोग", correct: false},
+        ] 
+    },
+
+
+    
+ 
+   
+];
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    });
+} 
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz(); 
